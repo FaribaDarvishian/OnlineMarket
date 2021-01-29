be092b1fd22758516e23066878aae360aa682523
fariba <fariba.darvishyan1994@gmail.com>
2021-01-29 10:58:43 +0330

    Refactor *Move sever request from CustomerRepository to view models. *Move sever request from CategoryRepository to view models.

--- app/src/main/java/com/example/digikala/data/repository/CustomerRepository.java
+++ app/src/main/java/com/example/digikala/data/repository/CustomerRepository.java
@@ -20,8 +20,7 @@ import retrofit2.Response;
 
 public class CustomerRepository {
     private static CustomerRepository sInstance;
-    private final MutableLiveData<Customer> mCustomerLiveData;
-    //    private final MutableLiveData<Boolean> mRegisterCustomerLiveData;
+
     private WooCommerceAPI mWooCommerceAPI;
 
     public static CustomerRepository getCustomer() {
@@ -32,60 +31,16 @@ public class CustomerRepository {
 
     private CustomerRepository() {
         mWooCommerceAPI = RetrofitInstance.getInstance().create(WooCommerceAPI.class);
-        mCustomerLiveData = new MutableLiveData<>();
-//        mRegisterCustomerLiveData = new MutableLiveData<>();
+
     }
 
-    public LiveData<Customer> getCustomerLiveData() {
-        return mCustomerLiveData;
-    }
 
-/*    public MutableLiveData<Boolean> getRegisterCustomerLiveData() {
-        return mRegisterCustomerLiveData;
-    }*/
-
-    public void setCustomerLiveData(String email) {
+        public Call<List<Customer>> setCustomerLiveData(String email) {
         Log.d(ProfileFragment.TAG, "onResponse: setCustomer " + email);
-        mWooCommerceAPI.getCustomers(NetworkParams.getCustomer(email))
-                .enqueue(new Callback<List<Customer>>() {
-                    @Override
-                    public void onResponse(Call<List<Customer>> call, Response<List<Customer>> response) {
-//                        Log.d(ProfileFragment.TAG, "onResponse: size " + response.body().size());
-//                        Log.d(ProfileFragment.TAG, "onResponse: email " + response.body().get(0).getEmail());
-                        if (response.body() != null && response.body().size() > 0)
-                            mCustomerLiveData.setValue(response.body().get(0));
-                        else
-                            registerCustomer(email);
-                    }
-
-                    @Override
-                    public void onFailure(Call<List<Customer>> call, Throwable t) {
-
-                    }
-                });
+            return mWooCommerceAPI.getCustomers(NetworkParams.getCustomer(email));
     }
 
-    public void registerCustomer(String email) {
-        Customer customer = new Customer();
-        customer.setEmail(email);
-        mWooCommerceAPI.postCustomers(new HashMap<>(), customer)
-                .enqueue(new Callback<Customer>() {
-                    @Override
-                    public void onResponse(Call<Customer> call, Response<Customer> response) {
-                        Log.d(ProfileFragment.TAG, "onResponse: Register Customer " + response.isSuccessful());
-                        Log.d(ProfileFragment.TAG, "onResponse: Register Customer " + response.message());
-                        if (response.isSuccessful())
-                            setCustomerLiveData(email);
-                    }
-
-                    @Override
-                    public void onFailure(Call<Customer> call, Throwable t) {
-
-                    }
-                });
-    }
-
-    public void signOut() {
-        mCustomerLiveData.setValue(null);
+    public Call<Customer> registerCustomer(Customer customer) {
+        return mWooCommerceAPI.postCustomers(new HashMap<>(), customer);
     }
 }
--- app/src/main/java/com/example/digikala/viewmodel/ProfileViewModel.java
+++ app/src/main/java/com/example/digikala/viewmodel/ProfileViewModel.java
@@ -1,6 +1,7 @@
 package com.example.digikala.viewmodel;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -10,16 +11,26 @@ import androidx.lifecycle.MutableLiveData;
 import com.example.digikala.data.model.customer.Customer;
 import com.example.digikala.data.repository.CustomerRepository;
 import com.example.digikala.utils.QueryPreferences;
+import com.example.digikala.view.fragment.ProfileFragment;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 
 public class ProfileViewModel extends AndroidViewModel {
     private CustomerRepository mRepository;
-    LiveData<Customer> mCustomerLiveData;
+    private MutableLiveData<Customer> mCustomerLiveData;
 
     public ProfileViewModel(@NonNull Application application) {
         super(application);
         mRepository = CustomerRepository.getCustomer();
         mCustomerLiveData = new MutableLiveData<>();
-        mCustomerLiveData = mRepository.getCustomerLiveData();
+
     }
 
     public LiveData<Customer> getCustomerLiveData() {
@@ -27,14 +38,51 @@ public class ProfileViewModel extends AndroidViewModel {
     }
 
     public void setCustomerLiveData(String email) {
-        mRepository.setCustomerLiveData(email);
+        mRepository.setCustomerLiveData(email)
+                .enqueue(new Callback<List<Customer>>() {
+        @Override
+        public void onResponse(Call<List<Customer>> call, Response<List<Customer>> response) {
+//                        Log.d(ProfileFragment.TAG, "onResponse: size " + response.body().size());
+//                        Log.d(ProfileFragment.TAG, "onResponse: email " + response.body().get(0).getEmail());
+            if (response.body() != null && response.body().size() > 0)
+                mCustomerLiveData.setValue(response.body().get(0));
+            else
+                registerCustomer(email);
+        }
+                    public void registerCustomer(String email) {
+                        Customer customer = new Customer();
+                        customer.setEmail(email);
+                        mRepository.registerCustomer(customer)
+                                .enqueue(new Callback<Customer>() {
+                                    @Override
+                                    public void onResponse(Call<Customer> call, Response<Customer> response) {
+                                        Log.d(ProfileFragment.TAG, "onResponse: Register Customer " + response.isSuccessful());
+                                        Log.d(ProfileFragment.TAG, "onResponse: Register Customer " + response.message());
+                                        if (response.isSuccessful())
+                                            setCustomerLiveData(email);
+                                    }
+
+                                    @Override
+                                    public void onFailure(Call<Customer> call, Throwable t) {
+
+                                    }
+                                });
+                    }
+
+        @Override
+        public void onFailure(Call<List<Customer>> call, Throwable t) {
+
+        }
+    });
     }
 
     public boolean isCustomerReady() {
-        return mCustomerLiveData.getValue() == null ? false : true;
+
+        return QueryPreferences.getCustomerEmail(getApplication()) == null ? false : true;
+//        return mCustomerLiveData.getValue() == null ? false : true;
     }
     public void signOut() {
-        mRepository.signOut();
+        mCustomerLiveData.setValue(null);
         QueryPreferences.setCustomerId(getApplication(), -1);
     }
 }
